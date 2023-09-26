a24ebef219901de5ea8a5739ae9743806e02e4a6
filish_alt <filagotshewafera@gmail.com>
2023-09-26 11:18:54 +0300

    cc

--- ECX.HR.Application/CQRS/OtherLeaveBalance/Handler/Queries/GetOtherLeaveBalanceListRequestHandler.cs
+++ ECX.HR.Application/CQRS/OtherLeaveBalance/Handler/Queries/GetOtherLeaveBalanceListRequestHandler.cs
@@ -116,8 +116,7 @@ namespace ECX.HR.Application.CQRS.OtherLeaveBalance.Handler.Queries
                 int annualLeave = Math.Min(accruedLeave, maxLeaveDays);
 
                 LeaveBalance.AnnualDefaultBalance = annualLeave;
-                decimal p2y = Math.Max(0, LeaveBalance.PreviousTwoYear - LeaveBalance.AnnualDefaultBalance);
-                LeaveBalance.PreviousTwoYear = p2y;
+               
                 var annualRemaining = LeaveBalance.PreviousYearAnnualBalance + LeaveBalance.PreviousTwoYear + LeaveBalance.AnnualDefaultBalance;
               
 
